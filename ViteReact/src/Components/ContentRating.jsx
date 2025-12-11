fbd14801f10c7fe2ceaca238e450e29652c32b30
soumyaranjandas24 <soumyaranjan24@outlook.com>
2025-12-11 13:35:08 -0500

    adding total rating in ContentRating.jsx

--- ViteReact/src/Components/ContentRating.jsx
+++ ViteReact/src/Components/ContentRating.jsx
@@ -3,16 +3,38 @@ import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            likes: 0,
+            dislikes: 0,
+            total: 0,
+            handleLike: () => {
+                this.setState((prevState) => ({ likes: prevState.likes + 1, total: prevState.total + 1 }))
+            },
+            handleDislike: () => {
+                this.setState((prevState) => ({ dislikes: prevState.dislikes + 1, total: prevState.total + 1 }))
+            }
+        }
+    }
+    render() {
+        return (
+            <>
+                <div className='content-rating'>
+                    <p>This is Paragrph</p>
+                    <div className="rating-buttons">
+                        <button className="like-button" onClick={this.state.handleLike}>
+                            Like ({this.state.likes})
+                        </button>
+                        <button className="like-button" onClick={this.state.handleDislike}>
+                            Dislike ({this.state.dislikes})
+                        </button>
+                    </div>
+                    <p>The Total Rating is ({this.state.total})</p>
+                </div>
+            </>
+        );
+    }
 }
 
 export default ContentRating;
