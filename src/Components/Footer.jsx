a760f10d4d41d1b0f16d0650a2371d1ff8d88209
soumyaranjandas24 <soumyaranjan24@outlook.com>
2025-12-12 10:36:20 -0500

    Footer added

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './EventPlanner.jsx'
+
+const Footer = () => {
+    return (
+        <footer>
+            <section>
+                <p>&copy;Event Planner Organisation, All right reserved.</p>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer
